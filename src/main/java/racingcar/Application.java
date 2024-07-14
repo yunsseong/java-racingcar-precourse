fe078c455228743d5972370bbdc4228a90058678
yunsseong <aster20@hanyang.ac.kr>
2024-07-14 21:13:31 +0900

    feat(Application): init and run gameController

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.model.Car;
+import racingcar.model.CarManager;
+import racingcar.view.Message;
+
 public class Application {
+    private static GameController gameController;
     public static void main(String[] args) {
-        // TODO 구현 진행
+        GameController gameController = new GameController(new CarManager(), new Message());
+        gameController.init();
+        gameController.run();
     }
 }
