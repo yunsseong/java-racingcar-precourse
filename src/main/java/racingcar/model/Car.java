c90305ca456121dce8eb145cc1f2804cc6376b5e
yunsseong <aster20@hanyang.ac.kr>
2024-07-15 18:08:02 +0900

    feat(Car): forward car method implement

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,14 +1,25 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    public void forward(){
-        position++;
-    }
+	public void forward() {
+		if (Randoms.pickNumberInRange(0, 9) >= 4)
+			position++;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public int getPosition(){
+		return position;
+	}
 }
