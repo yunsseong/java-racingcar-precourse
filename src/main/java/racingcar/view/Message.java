a7a19ec478d940329bd1258362ec21c2d170da98
yunsseong <aster20@hanyang.ac.kr>
2024-07-15 11:29:16 +0900

    feat(Message): validate trail number feature implement

--- src/main/java/racingcar/view/Message.java
+++ src/main/java/racingcar/view/Message.java
@@ -2,27 +2,38 @@ package racingcar.view;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class Message {
-	public String scan(){
+	private static final Pattern digitPattern = Pattern.compile("^[1-9]*$");
+	public String scan() {
 		return Console.readLine();
 	}
 
-	public String[] nameSeparate(String userInput){
+	public String[] nameSeparate(String userInput) {
 		return userInput.split(",");
 	}
 
-	public Boolean lengthOver(String string){
+	public boolean lengthOver(String string) {
 		return Arrays.stream(nameSeparate(string))
 			.anyMatch(s -> s.length() > 5);
 	}
 
 	public void validateCarName(String userInput) throws IllegalArgumentException{
 		if(userInput.isEmpty())
+	public void validateCarName(String userInput) throws IllegalArgumentException {
+		if (userInput.isEmpty())
 			throw new IllegalArgumentException("자동차 이름을 입력해주세요");
-		if(lengthOver(userInput))
+		if (lengthOver(userInput))
 			throw new IllegalArgumentException("자동차 이름을 5글자 이하로 입력해주세요");
+	}
+
+	public void validateTrialNum(String userInput) throws IllegalArgumentException{
+		if(userInput.isEmpty())
+			throw new IllegalArgumentException("시도 횟수를 꼭 입력해야해요");
+		if(!userInput.matches(digitPattern.pattern()))
+			throw new IllegalArgumentException("시도 횟수로 숫자를 입력해주세요");
 	}
 }
