4cc805511cd2d0b6dc9d72b83dcc220c90618972
yunsseong <aster20@hanyang.ac.kr>
2024-07-15 18:09:07 +0900

    feat(CarRacing): get winner list feature implement

--- src/main/java/racingcar/model/CarRacing.java
+++ src/main/java/racingcar/model/CarRacing.java
@@ -1,12 +1,34 @@
 package racingcar.model;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class CarRacing {
-	public List<Car> generateCarList(List<String> carNameList){
-		return carNameList.stream()
+	private List<Car> carList;
+
+	public void generateCarList(List<String> carNameList) {
+		carList = carNameList.stream()
 			.map(Car::new)
 			.collect(Collectors.toList());
+	}
+
+	public void forwardCar() {
+		carList.forEach(Car::forward);
+	}
+
+	public List<Car> getCarList() {
+		return carList;
+	}
+
+	public List<Car> getWinnerList() throws NoSuchElementException{
+		Car maxCar = Optional.ofNullable(carList.stream()
+			.max(Comparator.comparingInt(Car::getPosition))
+				.orElseThrow(NoSuchElementException::new))
+			.get();
+		return carList.stream()
+			.filter(c -> c.getPosition() == maxCar.getPosition()).collect(Collectors.toList());
 	}
 }
